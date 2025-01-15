64fcc2d639efdc019f13df63e286c90eed1fc30c
Zavierzs <zslee0406@gmail.com>
2025-01-15 17:54:51 +0800

    minor changes to cart

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,7 +1,47 @@
+import React from "react";
+import "./Cart.css";
+import { Header } from "../components";
+
+
 export const Cart = () => {
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="shopping-cart-container">
+      <Header />   
+      <div className="empty-basket">
+        <div className="empty-icon">
+          <i className="fa fa-shopping-bag"></i>
+        </div>
+        <p>Your basket is empty</p>
+        <p>The items you add will be shown here</p>
+      </div>
+
+      {/* Suggestions Section */}
+      <div className="suggestions">
+        <h2>YOU MAY ALSO LIKE</h2>
+        <div className="suggestions-items">
+          {/* Sample items, replace these with dynamic content */}
+          <div className="suggestion-item">
+            <img src="/images/sample-perfume.jpg" alt="Perfume" />
+            <p>RED ZARA TEMPTATION</p>
+          </div>
+          <div className="suggestion-item">
+            <img src="/images/sample-jeans.jpg" alt="Jeans" />
+            <p>WIDE-LEG JEANS</p>
+          </div>
+          <div className="suggestion-item">
+            <img src="/images/sample-coat.jpg" alt="Coat" />
+            <p>BLACK COAT</p>
+          </div>
+          <div className="suggestion-item">
+            <img src="/images/sample-perfume2.jpg" alt="Perfume" />
+            <p>GOLDEN DELUXE</p>
+          </div>
+          <div className="suggestion-item">
+            <img src="/images/sample-jacket.jpg" alt="Leather Jacket" />
+            <p>LEATHER JACKET</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
