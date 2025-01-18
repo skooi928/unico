96a656660c2bb486e29bb2f29570281e351be6c4
Jun <junliang1115@gmail.com>
2025-01-18 17:35:38 +0800

    fix the bug on sidebar

--- frontend/src/components/Product/Sidebar.jsx
+++ frontend/src/components/Product/Sidebar.jsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Sidebar.css';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa'; // Import arrows from react-icons
 
 const Sidebar = ({ onPriceSelect }) => {
     const [expanded, setExpanded] = useState({ gender: false, price: false });
+    const [selectedPriceRange, setSelectedPriceRange] = useState([0, Infinity]);
+    const location = useLocation();
 
     const toggleMenu = (menu) => {
         setExpanded((prev) => ({ ...prev, [menu]: !prev[menu] }));
     };
+
+    const isActive = (hash) => location.hash === hash;
+
+    const handlePriceSelect = (min, max) => {
+        setSelectedPriceRange([min, max]);
+        onPriceSelect(min, max);
+    };
+
+    const isPriceActive = (min, max) => selectedPriceRange[0] === min && selectedPriceRange[1] === max;
 
     return (
         <div className="sidebar">
@@ -33,16 +44,36 @@ const Sidebar = ({ onPriceSelect }) => {
                     {expanded.gender && (
                         <ul className="sidebar-submenu">
                             <li className="sidebar-subitem">
-                                <NavLink to="/product#All" activeClassName="active-link">All</NavLink>
+                                <NavLink
+                                    to="/product#All"
+                                    className={isActive('#All') ? 'active-link' : ''}
+                                >
+                                    All
+                                </NavLink>
                             </li>
                             <li className="sidebar-subitem">
-                                <NavLink to="/product#Men" activeClassName="active-link">Men</NavLink>
+                                <NavLink
+                                    to="/product#Men"
+                                    className={isActive('#Men') ? 'active-link' : ''}
+                                >
+                                    Men
+                                </NavLink>
                             </li>
                             <li className="sidebar-subitem">
-                                <NavLink to="/product#Women" activeClassName="active-link">Women</NavLink>
+                                <NavLink
+                                    to="/product#Women"
+                                    className={isActive('#Women') ? 'active-link' : ''}
+                                >
+                                    Women
+                                </NavLink>
                             </li>
                             <li className="sidebar-subitem">
-                                <NavLink to="/product#Kids" activeClassName="active-link">Kids</NavLink>
+                                <NavLink
+                                    to="/product#Kids"
+                                    className={isActive('#Kids') ? 'active-link' : ''}
+                                >
+                                    Kids
+                                </NavLink>
                             </li>
                         </ul>
                     )}
@@ -67,19 +98,64 @@ const Sidebar = ({ onPriceSelect }) => {
                     {expanded.price && (
                         <ul className="sidebar-submenu">
                             <li className="sidebar-subitem">
-                                <a href="#" onClick={() => onPriceSelect(0, Infinity)} className="price-link">All</a>
+                                <NavLink
+                                    to="#price-all"
+                                    className={isPriceActive(0, Infinity) ? 'active-link' : ''}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        handlePriceSelect(0, Infinity);
+                                    }}
+                                >
+                                    All
+                                </NavLink>
                             </li>
                             <li className="sidebar-subitem">
-                                <a href="#" onClick={() => onPriceSelect(0, 49.99)} className="price-link">RM 0 - RM 49.99</a>
+                                <NavLink
+                                    to="#price-0-49"
+                                    className={isPriceActive(0, 49.99) ? 'active-link' : ''}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        handlePriceSelect(0, 49.99);
+                                    }}
+                                >
+                                    RM 0 - RM 49.99
+                                </NavLink>
                             </li>
                             <li className="sidebar-subitem">
-                                <a href="#" onClick={() => onPriceSelect(50, 99.99)} className="price-link">RM 50 - RM 99.99</a>
+                                <NavLink
+                                    to="#price-50-99"
+                                    className={isPriceActive(50, 99.99) ? 'active-link' : ''}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        handlePriceSelect(50, 99.99);
+                                    }}
+                                >
+                                    RM 50 - RM 99.99
+                                </NavLink>
                             </li>
                             <li className="sidebar-subitem">
-                                <a href="#" onClick={() => onPriceSelect(100, 149.99)} className="price-link">RM 100 - RM 149.99</a>
+                                <NavLink
+                                    to="#price-100-149"
+                                    className={isPriceActive(100, 149.99) ? 'active-link' : ''}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        handlePriceSelect(100, 149.99);
+                                    }}
+                                >
+                                    RM 100 - RM 149.99
+                                </NavLink>
                             </li>
                             <li className="sidebar-subitem">
-                                <a href="#" onClick={() => onPriceSelect(150, Infinity)} className="price-link">RM 150 and above</a>
+                                <NavLink
+                                    to="#price-150"
+                                    className={isPriceActive(150, Infinity) ? 'active-link' : ''}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        handlePriceSelect(150, Infinity);
+                                    }}
+                                >
+                                    RM 150 and above
+                                </NavLink>
                             </li>
                         </ul>
                     )}
